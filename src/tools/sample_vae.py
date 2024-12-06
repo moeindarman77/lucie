fcaf18f7e2be4a8cddf420f4679b2382b5150695
moeindarman77 <moein.darman77@gmail.com>
2024-12-06 11:06:01 -0700

    Modifying to match the most recent training script

--- src/tools/sample_vae.py
+++ src/tools/sample_vae.py
@@ -9,13 +9,15 @@ import yaml
 from torch.utils.data.dataloader import DataLoader
 from torchvision.utils import make_grid
 from tqdm import tqdm
-
+from dataset.ClimateDataset import ClimateDataset
 from dataset.celeb_dataset import CelebDataset
 from dataset.mnist_dataset import MnistDataset
 from models.vqvae import VQVAE
 from models.vae import VAE
-from dataset.utilities import GlorysRomsDataset
+from dataset.ClimateDataset import ClimateDataset
 import torch.nn.functional as F
+from torch.nn import DataParallel
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # device = 'cpu'
@@ -43,16 +45,37 @@ def infer(args):
     if not os.path.exists(task_dir):
         os.mkdir(task_dir)
 
-    steps = range(9130, 10226)  # Some example steps 
-    data_dir = "/home/exouser/qgm1/fujitsu_data"
-
     # Create dataset instance
-    dataset = GlorysRomsDataset(steps=range(dataset_config['num_train_data']),
-                                 channels=["SSU", "SSV", "SSH",], 
-                                 added_channels=[], 
-                                 data_dir=dataset_config["data_dir"],
-                                 lat_lon_keep= tuple(dataset_config["lat_lon_keep"]), 
-                                 interpolator_use="scipy", )
+    input_vars = ['Temperature_7', 'Specific_Humidity_7', 'U-wind_3', 'V-wind_3', 'logp', 'tp6hr']
+    output_vars = ['2m_temperature', 'tp6hr']
+    lr_lats = [87.159, 83.479, 79.777, 76.070, 72.362, 68.652, 64.942, 61.232, 
+           57.521, 53.810, 50.099, 46.389, 42.678, 38.967, 35.256, 31.545, 
+           27.833, 24.122, 20.411, 16.700, 12.989, 9.278, 5.567, 1.856, 
+           -1.856, -5.567, -9.278, -12.989, -16.700, -20.411, -24.122, 
+           -27.833, -31.545, -35.256, -38.967, -42.678, -46.389, -50.099, 
+           -53.810, -57.521, -61.232, -64.942, -68.652, -72.362, -76.070, 
+           -79.777, -83.479, -87.159]
+
+    lr_lons = [0.0, 3.75, 7.5, 11.25, 15.0, 18.75, 22.5, 26.25, 30.0, 33.75,
+           37.5, 41.25, 45.0, 48.75, 52.5, 56.25, 60.0, 63.75, 67.5, 71.25,
+           75.0, 78.75, 82.5, 86.25, 90.0, 93.75, 97.5, 101.25, 105.0, 108.75,
+           112.5, 116.25, 120.0, 123.75, 127.5, 131.25, 135.0, 138.75, 142.5, 146.25,
+           150.0, 153.75, 157.5, 161.25, 165.0, 168.75, 172.5, 176.25, 180.0, 183.75,
+           187.5, 191.25, 195.0, 198.75, 202.5, 206.25, 210.0, 213.75, 217.5, 221.25,
+           225.0, 228.75, 232.5, 236.25, 240.0, 243.75, 247.5, 251.25, 255.0, 258.75,
+           262.5, 266.25, 270.0, 273.75, 277.5, 281.25, 285.0, 288.75, 292.5, 296.25,
+           300.0, 303.75, 307.5, 311.25, 315.0, 318.75, 322.5, 326.25, 330.0, 333.75,
+           337.5, 341.25, 345.0, 348.75, 352.5, 356.25]
+    dataset = ClimateDataset(input_dir_lr=dataset_config['input_data_dir'], 
+                             input_dir_hr=dataset_config['output_data_dir'], 
+                             input_vars=input_vars, 
+                             output_vars=output_vars, 
+                             lr_lats=lr_lats, 
+                             lr_lons=lr_lons,
+                             year_range=(dataset_config['year_range_start'], dataset_config['year_range_end']),
+                             normalize=True, 
+                             input_normalization_file=dataset_config['input_normalization_dir'], 
+                             output_normalization_file=dataset_config['output_normalization_dir']) 
 
     # Create DataLoader
     data_loader = DataLoader(dataset, 
@@ -60,61 +83,37 @@ def infer(args):
                              shuffle=False, 
                              num_workers=2)
 
-    model = VAE(im_channels=dataset_config['im_channels'],
-                  model_config=autoencoder_config).to(device)
+    model = VAE(input_channels=dataset_config['input_channels'],
+                output_channels=dataset_config['output_channels'],
+                model_config=autoencoder_config)
+    model = DataParallel(model)
+    model = model.to(device)
+
     # load_dir = os.path.join(task_dir, train_config['vae_autoencoder_ckpt_name'])
-    load_dir = '/media/volume/moein-storage-1/ddpm_ocean/results/spectral_loss_v3/vae_autoencoder_ckpt.pth'
-    model.load_state_dict(torch.load(load_dir, 
-                                    map_location=device)['model_state_dict'])
+    load_dir = '/glade/derecho/scratch/mdarman/lucie/results/vae_concat_v0/checkpoints/vae_autoencoder_ckpt_epoch_40.pth'
+    model.load_state_dict(torch.load(load_dir, map_location=device, weights_only=True)['model_state_dict'])
     model.eval()
     step = 0
     with torch.no_grad():
         for data in data_loader:
             step += 1
-            lres, hres = data
+            lres, hres = data['input'], data['output']
             lres = lres.float().to(device)
             hres = hres.float().to(device)
-
-            # hres = F.pad(hres, (3, 4, 2, 2), mode='constant', value=0)
-            # lres_upsampled = F.interpolate(lres, size=(352, 608), mode='bilinear', align_corners=False)
-        
-            # encoded_output, _, out_for_encoder = model.encode(lres)
-            # decoded_output = model.decode(encoded_output, out_for_encoder)
-            
+                        
             decoded_output, _ = model(lres)
+            decoded_output = decoded_output[:, :, :-7, :] 
 
             # Convert tensors to numpy arrays
+            lres_interp_numpy = F.interpolate(lres, size=(721,1440), mode='bilinear', align_corners=True).cpu().numpy()
+            lres_numpy = lres.cpu().numpy()            # Shape: [batch_size, channels, height, width]
             hres_numpy = hres.cpu().numpy()            # Shape: [batch_size, channels, height, width]
             decoded_outputs_numpy = decoded_output.cpu().numpy()   # Shape: [num_samples, batch_size, channels, height, width]
 
             # Save to an npz file named with the index of the data loader
             save_dir = os.path.join(task_dir,'samples', f'{step}.npz')
-            np.savez(save_dir, hres=hres_numpy, decoded_outputs=decoded_outputs_numpy)
+            np.savez(save_dir, lres=lres_numpy, lres_interp=lres_interp_numpy, hres=hres_numpy, output=decoded_outputs_numpy)
             print(f'Saved {step}.npz')
-        # encoded_output = torch.clamp(encoded_output, -1., 1.)
-        # encoded_output = (encoded_output + 1) / 2
-        # decoded_output = torch.clamp(decoded_output, -1., 1.)
-        # decoded_output = (decoded_output + 1) / 2
-        # ims = (ims + 1) / 2
-
-        # encoder_grid = make_grid(encoded_output.cpu(), nrow=ngrid)
-        # decoder_grid = make_grid(decoded_output.cpu(), nrow=ngrid)
-        # input_grid = make_grid(ims.cpu(), nrow=ngrid)
-        # encoder_grid = torchvision.transforms.ToPILImage()(encoder_grid)
-        # decoder_grid = torchvision.transforms.ToPILImage()(decoder_grid)
-        # input_grid = torchvision.transforms.ToPILImage()(input_grid)
-        
-        # input_grid.save(os.path.join(train_config['task_name'], 'input_samples.png'))
-        # encoder_grid.save(os.path.join(train_config['task_name'], 'encoded_samples.png'))
-        # decoder_grid.save(os.path.join(train_config['task_name'], 'reconstructed_samples.png'))
-
-    # 
-    # num_images = train_config['num_samples']
-    # ngrid = train_config['num_grid_rows']
-    
-    # idxs = torch.randint(0, len(dataset) - 1, (num_images,))
-    # ims = torch.cat([dataset[idx][None, :] for idx in idxs]).float()
-    # ims = ims.to(device)
   
         if train_config['save_latents']:
             # save Latents (but in a very unoptimized way)
